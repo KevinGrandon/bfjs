eff3df4d49da5b4cba2ac2c74a4e2a3b32af2fa6
Kevin Grandon <kevingrandon@yahoo.com>
2015-01-12 12:30:56 -0500

    Tune genetic algorithm and fitness method.

--- src/fitness/string_fitness.js
+++ src/fitness/string_fitness.js
@@ -6,13 +6,16 @@ module.exports = function(str1, target) {
   }
 
   for (var i = 0; i < target.length; i++) {
-    if (!str1[i]) {
-    	fitness += 256;
-    	continue;
+    if (!str1[i] ) {
+      fitness += 256;
+      continue;
     }
-
-    fitness += 256 - Math.abs(str1.charCodeAt(i) - target.charCodeAt(i));
+    fitness += /*256 - */Math.abs(str1.charCodeAt(i) - target.charCodeAt(i));
+    //console.log('Step is:', fitness)
   }
-
-  return fitness;
+//console.log('NEW FIT', fitness)
+  return (256*target.length) - fitness;
 };
+
+//console.log('Fitness is (ÿ): ', module.exports('ÿÿÿÿÿÿÿÿÿÿÿ', 'Hello world'));
+//console.log('Fitness is (H): ', module.exports('Hello world', 'Hello world'));
